0fa3ff4dc7fdd291d88215f06d572f7d446f7ac3
rahulrangi18 <rahulrangi18@gmail.com>
2021-07-12 01:08:27 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -4,27 +4,22 @@ import Card from './Cards';
 import "./index.css";
 import Sdata from "./Sdata";
 
+
+function ncard(val)
+{
+  return (
+    <Card
+      title={val.title}
+      imgsrc={val.imgsrc}
+      sname={val.sname}
+      link={val.link}
+    />
+  );
+}
 ReactDOM.render(
   <>
     <h1 className="heading_style"> MY FAVORITE 3 NETFLIX WEBSERIES</h1>
-    <Card
-      title={Sdata[0].title}
-      imgsrc={Sdata[0].imgsrc}
-      sname={Sdata[0].sname}
-      link={Sdata[0].link}
-    />
-    <Card
-      title={Sdata[1].title}
-      imgsrc={Sdata[1].imgsrc}
-      sname={Sdata[1].sname}
-      link={Sdata[1].link}
-    />
-    <Card
-      title={Sdata[2].title}
-      imgsrc={Sdata[2].imgsrc}
-      sname={Sdata[2].sname}
-      link={Sdata[2].link}
-    />
+    {Sdata.map(ncard)}
   </>,
   document.getElementById("root")
 );
